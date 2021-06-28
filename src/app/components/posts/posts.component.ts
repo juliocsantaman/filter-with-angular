215cab89b0ca2429c204d6dc6aecaa406ee05b4f
juliocsantaman <juliocsantaman@gmail.com>
2021-06-28 13:00:36 -0500

    I modified data of my posts.

--- src/app/components/posts/posts.component.ts
+++ src/app/components/posts/posts.component.ts
@@ -10,33 +10,75 @@ export class PostsComponent implements OnInit {
   posts = [
     {
       id: 1,
-      title: 'Post uno',
-      date: '02/01/2021',
+      title: 'Aprendizaje',
+      date: '24/3/2021',
       link: 'https://juliocsantaman.github.io/learning.github.io/'
     },
     {
       id: 2,
-      title: 'Post dos',
-      date: '05/02/2021',
-      link: 'https://juliocsantaman.github.io/learning.github.io/'
+      title: 'Equalweb',
+      date: '25/3/2021',
+      link: 'https://juliocsantaman.github.io/rick-and-morty-api/'
     },
     {
       id: 3,
-      title: 'Post tres',
-      date: '03/03/2021',
-      link: 'https://juliocsantaman.github.io/learning.github.io/'
+      title: 'Stackable',
+      date: '29/3/2021',
+      link: 'https://drive.google.com/file/d/1a5L536BuVSkuLhQY6vWZ25GjvDsIz-u2/view?usp=sharing'
     },
     {
       id: 4,
-      title: 'Post cuatro',
-      date: '04/04/2021',
-      link: 'https://juliocsantaman.github.io/learning.github.io/'
+      title: 'Quizlet',
+      date: '31/3/2021',
+      link: 'https://juliocsantaman.github.io/flashcard.github.io/'
     },
     {
       id: 5,
-      title: 'Post cinco',
-      date: '08/05/2021',
-      link: 'https://juliocsantaman.github.io/learning.github.io/'
+      title: 'Wridea',
+      date: '31/3/2021',
+      link: 'https://juliocsantaman.github.io/flashcard.github.io/'
+    },
+    {
+      id: 6,
+      title: 'Whiteboard - Sketchpad',
+      date: '5/4/2021',
+      link: 'https://drive.google.com/file/d/1qQV3_yuO0auPnh5D_sYOrckG6sVaxb77/view?usp=sharing'
+    },
+    {
+      id: 7,
+      title: 'Natural Reader',
+      date: '21/6/2021',
+      link: 'https://drive.google.com/file/d/1VNrtXNrIYnC50MT8uFxTBM95nDocM_Ne/view?usp=sharing'
+    },
+    {
+      id: 8,
+      title: 'FromTextToSpeech',
+      date: '21/6/2021',
+      link: 'https://drive.google.com/file/d/1VNrtXNrIYnC50MT8uFxTBM95nDocM_Ne/view?usp=sharing'
+    },
+    {
+      id: 9,
+      title: 'Sonix',
+      date: '21/6/2021',
+      link: 'https://drive.google.com/file/d/1_F-jqZapnPgzCTfCkoOYQ-UrgboiQ8RC/view?usp=sharing'
+    },
+    {
+      id: 10,
+      title: 'Convertio',
+      date: '21/6/2021',
+      link: 'https://drive.google.com/file/d/1k8CtKRRiwmbAunPl3WOTy8CV_rHASQT5/view?usp=sharing'
+    },
+    {
+      id: 11,
+      title: 'Y2mate',
+      date: '21/6/2021',
+      link: 'https://drive.google.com/file/d/1k8CtKRRiwmbAunPl3WOTy8CV_rHASQT5/view?usp=sharing'
+    },
+    {
+      id: 12,
+      title: 'Watson',
+      date: '21/6/2021',
+      link: 'https://drive.google.com/file/d/1W55wwIs-xi2JPlfU3JGmSfu5Ccdg9Z8_/view?usp=sharing'
     }
   ]
 
--- src/app/pipes/filter.pipe.ts
+++ src/app/pipes/filter.pipe.ts
@@ -8,8 +8,9 @@ export class FilterPipe implements PipeTransform {
   transform(value: any, arg: any): any {
     const postResults = [];
     for(const post of value) {
-      if(post.title.indexOf(arg) > -1) {
+      if(post.title.toLowerCase().indexOf(arg.toLowerCase()) > -1) {
         postResults.push(post);
+        
       }
     }
 
